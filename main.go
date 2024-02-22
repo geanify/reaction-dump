9e747e67920a9b8c0da70d716a8ba9cac1d51252
geanify <geanify@gmail.com>
2024-02-22 18:44:20 +0200

    Added fyne for UI

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
 func _exec(str string) {
@@ -21,14 +29,14 @@ func _execOutput(str string) string {
 	out, err := cmd.Output()
 
 	if err != nil {
-		log.Fatal(err)
+		return ""
 	}
 	outStr := string(out[:])
 	return outStr
 }
 
-func copyImage(location string, filename string) {
-	str := "cat " + location + filename + "| xclip -selection clipboard -target image/png -i"
+func copyImage(path string) {
+	str := "cat " + path + "| xclip -selection clipboard -target image/png -i"
 	_exec(str)
 }
 
@@ -38,16 +46,67 @@ func textLookUp(text string) {
 	fmt.Println(out)
 }
 
+func createImage(path string) *widget.Button {
+	iconFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := bufio.NewReader(iconFile)
+
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	btn := widget.NewButtonWithIcon("Browse", fyne.NewStaticResource("icon", b), func() {
+		// do something
+		copyImage(path)
+	})
+	return btn
+}
+
+func imageList(images []string) []*widget.Button {
+	imageList := make([]*widget.Button, 0)
+	for i := 0; i < len(images); i++ {
+		newImage := createImage("reactions/test.jpg")
+		imageList = append(imageList, newImage)
+	}
+	return imageList
+}
+
+func render(window fyne.Window) {
+	search := widget.NewEntry()
+	search.OnChanged = textLookUp
+
+	img := createImage("reactions/test.jpg")
+	img2 := createImage("reactions/test.jpg")
+	// text := canvas.NewText("Overlay", color.Black)
+	// imgWidget := widget.NewCard("test", "test2", img)
+
+	content := container.New(layout.NewGridLayoutWithRows(3), search, img2, img, img)
+
+	window.SetContent(content)
+}
+
 func main() {
-	// location := ""
 	args := os.Args
 
 	if len(os.Args[1:]) < 1 {
-		copyImage("", "test.jpg")
+		copyImage("test.jpg")
 	} else {
-		copyImage(args[1], "test.jpg")
+		copyImage(args[1] + "test.jpg")
 	}
 	textLookUp("te")
+
+	myApp := app.New()
+	window := myApp.NewWindow("Reaction Dump")
+	render(window)
+
+	window.CenterOnScreen()
+	window.SetFixedSize(true)
+	window.Resize(fyne.NewSize(800, 600))
+	window.ShowAndRun()
 
 	// exec.Command("touch test.txt")
 }
