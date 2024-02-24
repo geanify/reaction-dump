b108c1b3c8417c2bb1347e68750287b8f1867f73
geanify <geanify@gmail.com>
2024-02-24 13:02:59 +0200

    show only first 30 images

--- utils.go
+++ utils.go
@@ -70,7 +70,7 @@ func textLookUp(text string) []string {
 	}
 
 	find := "find " + folder + " -type f \\( -iname \\*.jpg -o -iname \\*.png -o -iname \\*.gif -o -iname \\*.jpeg \\)"
-	str := find + " | grep " + text
+	str := find + "| head -30 | grep " + text
 	out := _execOutput(str)
 	fmt.Println(strings.Split(out, "\n"))
 	return strings.Split(out, "\n")
